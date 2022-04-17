d181042517df3c32b574fdd974c7a65ba2981fab
Daniel <satelerd@gmail.com>
2022-04-17 18:42:35 -0400

    its working!!!! omg

---
+++ src/pages/test.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './test.css';
+
+function Test() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                <h1>This is a TESTTTT</h1>
+                <p>
+                    Edit <code>src/App.js</code> and save to reloa
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Test;
